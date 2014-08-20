02f2bbfe3e928a4a52b6670a39d89c3abe5bab23
Utsav Shah <ukshah2@illinois.edu>
2014-08-20 20:27:41 +0530

    Added OrderClient.java

---
+++ src/com/steins/tradier/OrderClient.java
@@ -0,0 +1,79 @@
+package com.steins.tradier;
+
+import java.util.HashMap;
+
+import com.android.volley.Response;
+import com.android.volley.Request.Method;
+
+import android.content.Context;
+
+public class OrderClient extends Client {
+
+	private static final String BASE_URL = "https://api.tradier.com/v1";
+
+	protected static String requestUrl;
+
+	public OrderClient(String id, Context context, String token,
+			String contentType) {
+
+		super(context, token, contentType);
+
+		requestUrl += BASE_URL + "/accounts/" + id;
+	}
+
+	private void orderRequest(String requestType, int method,
+			Response.Listener<String> listener,
+			Response.ErrorListener errorListener,
+			HashMap<String, String> extraParameters) {
+
+		TradierRequest mRequest = new TradierRequest(listener, errorListener,
+				method, requestUrl + requestType, null, mHeaders,
+				extraParameters);
+
+		addToQueue(mRequest);
+
+	}
+
+	public void createOrder(Response.Listener<String> listener,
+			Response.ErrorListener errorListener,
+			HashMap<String, String> extraParameters) {
+
+		orderRequest("", Method.POST, listener, errorListener, extraParameters);
+
+	}
+
+	public void createMultilegOrder(Response.Listener<String> listener,
+			Response.ErrorListener errorListener,
+			HashMap<String, String> extraParameters) {
+
+		orderRequest("", Method.POST, listener, errorListener, extraParameters);
+
+	}
+
+	public void previewOrder(Response.Listener<String> listener,
+			Response.ErrorListener errorListener,
+			HashMap<String, String> extraParameters) {
+
+		extraParameters.put("preview", "true");
+
+		orderRequest("", Method.POST, listener, errorListener, extraParameters);
+
+	}
+
+	public void changeOrder(String orderID, Response.Listener<String> listener,
+			Response.ErrorListener errorListener,
+			HashMap<String, String> extraParameters) {
+
+		orderRequest("/" + orderID, Method.PUT, listener, errorListener,
+				extraParameters);
+
+	}
+
+	public void deleteOrder(String orderID, Response.Listener<String> listener,
+			Response.ErrorListener errorListener) {
+
+		orderRequest("/" + orderID, Method.DELETE, listener, errorListener,
+				null);
+	}
+
+}
