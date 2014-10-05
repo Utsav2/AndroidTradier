013e70b87521377ccd798dde441e044f53966f5a
Utsav Shah <ukshah2@illinois.edu>
2014-10-05 12:58:43 -0500

    Fixed bug in OrderClient

--- src/com/steins/tradier/OrderClient.java
+++ src/com/steins/tradier/OrderClient.java
@@ -18,7 +18,7 @@ public class OrderClient extends Client {
 
 		super(context, token, contentType);
 
-		requestUrl = BASE_URL + "/accounts/" + id;
+		requestUrl = BASE_URL + "/accounts/" + id + '/order';
 	}
 
 	private void orderRequest(String requestType, int method,
